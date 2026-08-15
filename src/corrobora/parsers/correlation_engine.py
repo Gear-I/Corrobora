5d72cfa0f40865745b0a9aa81054b4c88bd4c9af
Gear-I <dielleaiko@hotmail.com>
2026-08-15 19:25:33 -0400

    Fixed evtx import

--- src/corrobora/parsers/correlation_engine.py
+++ src/corrobora/parsers/correlation_engine.py
@@ -58,7 +58,7 @@ from datetime import datetime, timedelta
 from enum import Enum
 from pathlib import Path
 
-from evtx import EventRecord, EvtxFileError, EvtxParser
+from Evtx import EventRecord, EvtxFileError, EvtxParser
 from mft import MftFileError, MftParser, MftRecord
 from prefetch import (
     PrefetchFileError,
